a3c8d2b3bc254e4d1e20404135fb1cab64f47cb5
matenglovecoding <493384625@qq.com>
2016-12-21 18:36:12 +0800

    NIO_timeservice

---
+++ TimeService/src/TimeClient.java
@@ -0,0 +1,18 @@
+/**
+ * Created by t-tema on 12/20/2016.
+ */
+public class TimeClient {
+
+    public static void main(String[] args){
+        int port=8080;
+        if(args!=null&&args.length>0){
+            try{
+                port=Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+
+            }
+        }
+
+        new Thread(new TimeClientHandle("127.0.0.1",port),"TimeClient-001").start();
+    }
+}
---
+++ TimeService/src/TimeServer.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+/**
+ * Created by t-tema on 12/20/2016.
+ */
+public class TimeServer {
+
+    public static void main(String[] args)throws IOException{
+        int port=8080;
+        if(args!=null&&args.length>0){
+            try{
+                port=Integer.valueOf(args[0]);
+            }catch (NumberFormatException e){
+
+            }
+        }
+
+        MultiplexerTimeServer timeServer=new MultiplexerTimeServer(port);
+        new Thread(timeServer,"NIO-MultiplexerTimeServer-001").start();
+    }
+}
